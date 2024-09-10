d9c832a4b2d566a22b754d86ff463aff3fbf3d83
fazal wahab <wahabdir567@gmail.com>
2024-09-10 08:47:05 -0700

    size

--- src/subComponents/CarosalPerfume50ml.jsx
+++ src/subComponents/CarosalPerfume50ml.jsx
@@ -38,6 +38,7 @@ const description = [
 const Wrapper = styled.div`
   width: 21rem;
   height: 35vh;
+  // background-color: ${(props) => props.theme.text};
   color: ${(props) => props.theme.body};
   padding: 1.5rem 2rem;
   margin-right: 10rem;
@@ -45,6 +46,7 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  // border: 1px solid ${(props) => props.theme.body};
   transition: all 0.2s ease;
 `;
 
@@ -52,13 +54,13 @@ const Image = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
-  /* Adjusted the top margin to merge the image on top by 20px */
-  margin-top: 70px;
+  // transform:translate(-42%, -25%)
 
   @media screen and (max-width: 1200px) {
   }
 
   @media screen and (max-width: 768px) {
+    // transform: translate(-42%, -25%);
   }
 `;
 
@@ -89,8 +91,8 @@ const Div02a = styled.div`
 
 const ImageContainer = styled.div`
   position: absolute;
-  width: 250px;
-  height: auto;
+  width: 250px; /* Set the width of the image container */
+  height: auto; /* You can adjust the height as needed */
   transform: translate(70%, -20%);
 
   @media screen and (max-width: 1200px) {
@@ -117,10 +119,11 @@ const TextOverlay = styled.div`
 `;
 
 const Title2 = styled.h2`
-  color: white;
+  color: white; /* Set the text color */
   font-size: 0.7em;
   transform: translate(10%, 0%);
   width: 100px;
+  /* Add additional styles as needed */
 `;
 
 const Div1 = styled.div`
@@ -172,9 +175,12 @@ const Div3 = styled.div`
   gap: 105px;
 
   @media screen and (max-width: 1200px) {
+    // bottom: 0px;
+    // left: 0px;
   }
 
   @media screen and (max-width: 768px) {
+    // transform:translate(-12%, 95%);
   }
 `;
 
@@ -230,35 +236,45 @@ const Footer = styled.footer`
   transition: all 1s ease;
 
   @media screen and (max-width: 1200px) {
+    // bottom: 0px;
+    // left: 0px;
   }
 
   @media screen and (max-width: 768px) {
+    // transform: translate(180%, 10%);
   }
 `;
 
 const Link = styled.a`
+  // background: black;
   color: white;
-  z-index: 10;
+  z-index:10;
   box-shadow: 0 25px 50px -12px rgba(0, 0, 0, 0.25);
   &:focus {
     outline: none;
+    ring-4;
+    ring-cyan-300;
   }
-  &:hover {
-    box-shadow: 0 12px 20px -10px rgba(0, 0, 0, 0.2),
-      0 4px 20px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(0, 0, 0, 0.2);
+   &:hover {
+    // background: linear-gradient(to bottom right, #68d391, #4dabf7, #4299e1);
+    box-shadow: 0 12px 20px -10px rgba(0, 0, 0, 0.2), 0 4px 20px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(0, 0, 0, 0.2);
   }
   color: ${(props) => props.theme.text};
   text-decoration: none;
   padding: 0.5em;
   border-radius: 10px;
   font-size: 1.2em;
+
+  ${Wrapper}:hover & {
+   
+  }
 `;
 
 const Img = styled.img`
   width: 450px;
 
   @media screen and (max-width: 1200px) {
-    width: 400px;
+    width: 400px; // medium screens
   }
 
   @media screen and (max-width: 768px) {
